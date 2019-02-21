93153c90affd86730d044a390ef7115e27c37618
SSE4 <tomskside@gmail.com>
2019-02-22 03:24:23 +0700

    - some workarounds for MinGW
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -10,9 +10,22 @@ def run(command):
     if 0 != status:
         sys.exit(command)
 
+def remove_sh_from_path():
+    path = os.environ["PATH"]
+    path = path.split(os.pathsep)
+    for entry in path:
+        if os.path.isfile(os.path.join(entry, "sh.exe")):
+            path.remove(entry)
+    path = os.pathsep.join(path)
+    os.environ["PATH"] = path
+
 def main():
+    remove_sh_from_path()
     generator = os.environ["CMAKE_GENERATOR"]
-    run('cmake . -G "%s"' % generator)
+    command = 'cmake . -G "%s"' % generator
+    if "CMAKE_MAKE_PROGRAM" in os.environ:
+        command += "-D%s" % os.environ["CMAKE_MAKE_PROGRAM"]
+    run(command)
     run('cmake --build . --config Release')
 
 if __name__ == '__main__':
