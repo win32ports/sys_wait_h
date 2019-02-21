3f1806d1c167ef86f94122443841f6be5c262414
SSE4 <tomskside@gmail.com>
2019-02-22 03:05:43 +0700

    - add appveyor
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import os
+
+def main():
+    generator = os.environ["CMAKE_GENERATOR"]
+    os.system("cmake -G %s" % generator)
+    os.system("cmake --build . --config Release")
+
+if __name__ == '__main__':
+    main()
